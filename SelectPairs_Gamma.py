e406a0154ec6fb847a9f7f4ba79243657fc0528d
Yunmeng <ymcmrs@gmail.com>
2017-03-11 14:25:08 -0500

    choosing interferometry Pairs

---
+++ SelectPairs_Gamma.py
@@ -0,0 +1,58 @@
+#! /usr/bin/env python
+#'''
+###################################################################################
+#                                                                                 #
+#            Author:   Yun-Meng Cao                                               #
+#            Email :   ymcmrs@gmail.com                                           #
+#            Date  :   Mar, 2017                                                  #
+#                                                                                 #
+#         Select Interferometry-Pairs from time series SAR images                  #
+#                                                                                 #
+###################################################################################
+#'''
+import numpy as np
+import os
+import pysar._readfile as readfile
+import sys  
+import subprocess
+import getopt
+import time
+import glob
+
+
+def usage():
+    print '''
+******************************************************************************************************
+ 
+           Select interferometry pairs from time series SAR images
+     
+      usage:
+   
+            SelectPairs_Gamma.py ProjectName
+      
+      e.g.  SelectPairs_Gamma.py PacayaT163TsxHhA
+          
+            
+*******************************************************************************************************
+    '''   
+    
+def main(argv):
+    
+    if len(sys.argv)==2:
+        if argv[0] in ['-h','--help']: usage(); sys.exit(1)
+        else: ProjectName=sys.argv[1]        
+    else:
+        usage();sys.exit(1)
+        
+    scratchDir = os.getenv('SCRATCHDIR')
+    templateDir = os.getenv('TEMPLATEDIR')
+    templateFile = templateDir + "/" + projectName + ".template"
+    templateContents=readfile.read_template(templateFile)
+    processDir = scratchDir + '/' + projectName + "/PROCESS"
+    slcDir     = scratchDir + '/' + projectName + "/SLC"
+    
+    SLCfile=glob.glob(slcDir+'/*/*.slc')  
+    SLCParfile =glob.glob(slcDir+'/*/*.slc.par')  
+    
+    
+    
