dda98197443f82097525d8b137070758d55e49ac
Yunmeng <ymcmrs@gmail.com>
2017-03-11 17:39:50 -0500

    bug fix

--- SelectPairs_Gamma.py
+++ SelectPairs_Gamma.py
@@ -120,9 +120,8 @@ def main(argv):
         File.write(str(SLCfile[kk])+ ' '+str(SLCParfile[kk])+'\n')
         
     File.close()
-    
-    call_str = "$GAMMA_BIN/base_calc " + SLC_tab + " " + RefPar + " " + TS_Berp + " " + TS_Itab + " " + '1 0 ' + '- ' + MaxSB
-+ ' - ' + MaxTB
+     
+    call_str = "$GAMMA_BIN/base_calc " + SLC_tab + " " + RefPar + " " + TS_Berp + " " + TS_Itab + " " + '1 0 ' + '- ' + MaxSB + ' - ' + MaxTB
     os.system(call_str)
 
       
