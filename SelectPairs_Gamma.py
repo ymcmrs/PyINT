d6e912137812b33b663f60fec0d97b31768b08c5
Yunmeng <ymcmrs@gmail.com>
2017-03-11 17:33:11 -0500

    bug fix

--- SelectPairs_Gamma.py
+++ SelectPairs_Gamma.py
@@ -59,13 +59,35 @@ def main(argv):
     templateContents=readfile.read_template(templateFile)
     processDir = scratchDir + '/' + projectName + "/PROCESS"
     slcDir     = scratchDir + '/' + projectName + "/SLC"
+
+# define files    
     
+    SLC_Tab = processDir + "/SLC_Tab"
+    TS_Berp = processDir + "/TS_Berp"
+    TS_Itab = processDir + "/TS_Itab"
+    itab_type = '1'
+    pltflg = '0'
+    
+    if 'Max_Spacial_Baseline'  in templateContents: MaxSB=templateContents['Max_Spacial_Baseline']
+    else:
+        print "Max_Spacial_Baseline is not found in template!! "
+        print "500m is chosen as the threshold for spatial baseline!"
+        MaxSB = '500'
+        
+    if 'Max_Temporal_Baseline'  in templateContents: MaxTB=templateContents['Max_Temporal_Baseline']
+    else:
+        print "Max_Temporal_Baseline is not found in template!! "
+        print "500 days is chosen as the threshold for temporal baseline!"
+        MaxTB = '500'
+    
+    
+#  extract available SAR images slc and slc_par    
     ListSLC = os.listdir(slcDir)
     Datelist = []
     SLCfile = []
     SLCParfile = []
     
-    print "All of the available SAR acquisition date is :"  
+    print "All of the available SAR acquisition datelist is :"  
     for kk in range(len(ListSLC)):
         if is_number(ListSLC[kk]):
             Datelist.append(ListSLC[kk])
@@ -75,16 +97,34 @@ def main(argv):
             SLCfile.append(str_slc)
             SLCParfile.append(str_slc_par)
     
+    if 'masterDate'          in templateContents:
+        masterDate0 = templateContents['masterDate']
+        if masterDate0 in Datelist:
+            masterDate = masterDate0
+            print "masterDate : " + masterDate0
+        else:
+            masterDate=Datelist[0]
+            print "The selected masterDate is not included in above datelist !!"
+            print "The first date [ %s ] is chosen as the master date! " % Datelist[0] 
+            
+    else:  
+        masterDate=Datelist[0]
+        print "masterDate is not found in template!!! "
+        print "The first date [ %s ] is chosen as the master date! " % Datelist[0] 
 
-    File= open('base_calc_txt','w')
+    RefPar=slcDir + "/" + masterDate +"/" + masterDate + ".slc.par"
+       
+    File= open(SLC_tab,'w')
     
     for kk in range(len(SLCfile)):
         File.write(str(SLCfile[kk])+ ' '+str(SLCParfile[kk])+'\n')
         
-        
-    print SLCfile[1]
-    print SLCParfile[1]
+    File.close()
     
+    call_str = "$GAMMA_BIN/base_calc " + SLC_tab + " " + RefPar + " " + TS_Berp + " " + TS_Itab + " " + '1 0 ' + '- ' + MAXSB
++ ' - ' + MAXTB
+    os.system(call_str)
+
     
     
     
