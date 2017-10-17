f006ad985124321d5b62574b37bb2747af95aca7
Yunmeng Cao <ymcmrs@gmail.com>
2017-10-16 23:01:23 -0400

    Update GenerateRSC_Sen_Gamma.py

--- GenerateRSC_Sen_Gamma.py
+++ GenerateRSC_Sen_Gamma.py
@@ -134,6 +134,10 @@ def main(argv):
     SRSLCPAR = workDir + '/' + Sdate + Suffix[0] + '.rslc.par'    
     
     OFF   =  workDir + '/' + Mdate + '-' + Sdate + '_' + rlks + 'rlks' + Suffix[0] + '.off'
+    if not os.path.isfile(OFF):
+        call_str = 'create_offset ' + workDir+'/' + Mdate + '.rslc.par ' +  workDir+'/' + Sdate + '.rslc.par ' + ' 1 ' + rlks + ' ' + azlks + ' 0'
+        os.system(call_str)
+        
     BASE  =  workDir + '/' + Mdate + '-' + Sdate + Suffix[0] + '.base'
     PBASE =  workDir + '/' + Mdate + '-' + Sdate + Suffix[0] + '.base_perp'
     PBASE_TXT = workDir + '/' + Mdate + '-' + Sdate + Suffix[0] + '.base_perp_txt'
