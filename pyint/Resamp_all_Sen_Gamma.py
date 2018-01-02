80fc8f4f7e03146d553c63a099a74fa175836205
Yunmeng Cao <ymcmrs@gmail.com>
2018-01-02 16:25:39 -0500

    Update Resamp_all_Sen_Gamma.py

--- pyint/Resamp_all_Sen_Gamma.py
+++ pyint/Resamp_all_Sen_Gamma.py
@@ -247,7 +247,7 @@ def main(argv):
     call_str = '$GAMMA_BIN/sub_phase ' + INTlks + ' ' + SIMUNW + ' ' + DIFFpar + ' ' + DIFFINTlks + ' 1 0'
     os.system(call_str)   
     
-    os.remove(INT)
+    #os.remove(INT)
     
      #call_str = '$GAMMA_BIN/adapt_filt ' + DIFFINTlks + ' ' + DIFFINTFILTlks + ' ' + nWidth + ' ' + fFiltLengthDiff + ' ' + nFiltWindowDiff
      #os.system(call_str)
